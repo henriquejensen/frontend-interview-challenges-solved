99e5c70614f1fb2aec4ee23ef502b11e8b14d2e5
Henrique Jensen <henriquejensen@hotmail.com>
2019-06-20 19:12:25 -0300

    draw red circles at the user click

--- draw.js
+++ draw.js
@@ -5,12 +5,43 @@ function drawCircle(context, posX, posY) {
     context.fill();
 }
 
+
+
 window.onload = () => {
 
-    const canvas = document.getElementById("draw-canvas");    
-    canvas.addEventListener(
-        'click',
-        ({ offsetX, offsetY }) => drawCircle(canvas.getContext("2d"), offsetX, offsetY) 
-    );
+    const numberOfPoints = 3;
+    const arrayOfPoints = [];
+    const canvas = document.getElementById("draw-canvas");
+    const context = canvas.getContext("2d");
+    const container = {
+        x: 0,
+        y: 0,
+        width: canvas.width,
+        height: canvas.height
+    };
+    canvas.addEventListener('click', drawCircleController);
+
+    function drawCircleController({ offsetX, offsetY }) {
+    
+        if (arrayOfPoints.length === numberOfPoints) {
+            arrayOfPoints.pop();
+        }
+
+        arrayOfPoints.unshift([offsetX, offsetY]);
+        requestAnimationFrame(drawPoints)
+    }
+
+    function drawPoints() {
+        initScreen();
+        arrayOfPoints.map(points => drawCircle(context, ...points))
+    }
+
+    function initScreen() {
+        context.fillStyle = 'black';
+        context.strokeStyle = 'black';
+        context.fillRect(container.x,container.y,container.width,container.height);
+    }
+
+    initScreen();
 
 }
